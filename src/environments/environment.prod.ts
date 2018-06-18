7ade1457c69ce023968c50de439440cb14b532cf
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-06-18 10:28:06 -0300

    Primeiro commit. Subindo arquivos restantes da crianção do projeto.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDtQRErqhNsb_uSu-EvQvvAPTvDUpC3Xuc",
+    authDomain: "oshop-98056.firebaseapp.com",
+    databaseURL: "https://oshop-98056.firebaseio.com",
+    projectId: "oshop-98056",
+    storageBucket: "oshop-98056.appspot.com",
+    messagingSenderId: "91660606826"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDtQRErqhNsb_uSu-EvQvvAPTvDUpC3Xuc",
+    authDomain: "oshop-98056.firebaseapp.com",
+    databaseURL: "https://oshop-98056.firebaseio.com",
+    projectId: "oshop-98056",
+    storageBucket: "oshop-98056.appspot.com",
+    messagingSenderId: "91660606826"
+  }
 };
 
 /*
