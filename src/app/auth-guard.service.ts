595ef95d36beb595f72678f31edc3cf06de0533d
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-06-19 11:22:56 -0300

    S20 A292 Protegendo caminhos de usuários externos e/ou anonimos.

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,9 +1,21 @@
+import 'rxjs/add/operator/map';
+import { CanActivate, Router } from '@angular/router'
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard {
+export class AuthGuard implements CanActivate{
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate() {
+    return this.auth.user$.map(user => {
+      if (user) return true;
+
+      this.router.navigate(['/login']);
+      return false;
+    });
+  }
 }
